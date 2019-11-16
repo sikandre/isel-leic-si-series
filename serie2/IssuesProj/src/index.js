8a13310e16a844554cef424802404ea043efe81c
sikandre <sik.andre@gmail.com>
2019-11-16 11:31:15 +0000

    refactor

--- serie2/IssuesProj/src/index.js
+++ serie2/IssuesProj/src/index.js
@@ -1,4 +1,6 @@
-//npm install semantic-ui
+//npm i pug
+// npm i -D browser-sync
+
 
 const express = require('express')
 var path = require('path')
@@ -8,8 +10,21 @@ var cookieParser = require('cookie-parser');
 var jwt = require('jsonwebtoken');
 var HashMap = require('hashmap');
 
+
+
+app.set("view engine", "pug");
+
 const port = 3001;
-app.use(express.static(__dirname));
+
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'pug');
+
+
+let publicDir = path.join(__dirname, 'public');
+
+
 var map = new HashMap();
 
 // more info at:
@@ -29,16 +44,22 @@ const GIT_CLIENT_ID = "8440aaee2cd8100b1136";
 const GIT_CLIENT_SECRET = "e45df0fe9b0c45e85992dee198aff1ab54a94c20";
 
 
-let publicDir = path.join(__dirname, 'public');
 let state = Math.round(new Date().getTime()).toString();
 
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+    res.render("index", { title: "Home" });
+});
+
+/*
 app.get('/', function (req, res) {
 
 
-    res.sendFile(path.join(publicDir, 'index.html'));
+    res.sendFile(path.join(publicDir, 'indexIsel.html'));
 });
+
+ */
 
 const validateToken = (req, resp, next) => {
     let cookie =  req.cookies['token'];
@@ -47,7 +68,6 @@ const validateToken = (req, resp, next) => {
         let json_response = map.get(decrypt.email);
         let jwt_payload = jwt.decode(json_response.id_token);
         let sub = jwt_payload.sub;
-        console.log(sub);
         if (decrypt.sub === sub){
             resp.status(200);
             next();
@@ -61,6 +81,7 @@ const validateToken = (req, resp, next) => {
 
 app.get('/auth', validateToken, (req, resp) => {
    //teste para fazer auth com o git
+
 
 
     resp.send('done');
@@ -126,7 +147,7 @@ app.get('/googlecallback', (req, resp) => {
                 }
                 map.set(email, json_response);
                 //resp.sendFile(path.join(publicDir, 'GithubLogin.html'));
-                resp.redirect('/getRepos');
+                resp.redirect('/gitindex');
 
             }
         );
@@ -134,14 +155,16 @@ app.get('/googlecallback', (req, resp) => {
     }
 });
 
-app.get('/getrepos', /*validateToken,*/ (req, resp) => {
-    resp.sendFile(path.join(publicDir, 'GithubLogin.html'));
+app.get('/gitindex', validateToken, (req, resp, next) => {
+    console.log("email",next);
+    //resp.sendFile(path.join(publicDir, 'GithubLogin.html'));
+    resp.render("gitindex", { title: "Home" });
 
 
     //resp.send('done');
 
 });
-app.get('/logingit', /*validateToken,*/ (req, resp) => {
+app.get('/logingit', validateToken, (req, resp, next) => {
     resp.redirect(302,
         // authorization endpoint
         'https://github.com/login/oauth/authorize?'
@@ -152,17 +175,19 @@ app.get('/logingit', /*validateToken,*/ (req, resp) => {
         // parameter state should bind the user's session to a request/response
         + 'state=' + state + '&'
         // responde_type for "authorization code grant"
-        //+ 'response_type=code&'
+        // +'accept=json&'
         // redirect uri used to register RP
         + 'redirect_uri=http://localhost:3001/githubcallback');
 });
 
-app.get('/githubcallback', (req, resp) => {
+app.get('/githubcallback', (req, resp, next) => {
     if (req.query.state !== state) {
         resp.status(403);
         resp.sendFile(path.join(publicDir, 'Forbidden.html'));
     } else {
         //get token
+
+        resp.set('Content-Type', 'application/json');
         request.post(
             {
                 url: 'https://github.com/login/oauth/access_token',
@@ -172,7 +197,7 @@ app.get('/githubcallback', (req, resp) => {
                     client_id: GIT_CLIENT_ID,
                     client_secret: GIT_CLIENT_SECRET,
                     redirect_uri: 'http://localhost:3001/githubcallback',
-                    state: state
+                    state: state,
                 }
             },
 
@@ -182,6 +207,7 @@ app.get('/githubcallback', (req, resp) => {
                 }
                 //let json_response = JSON.parse(body);
                 console.log("aki",body);
+                //console.log("http", httpResponse);
                 /*
 
                 let jwt_payload = jwt.decode(json_response.id_token);
