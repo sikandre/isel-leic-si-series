2d3a5eb3a9b611c06942f3c82c258278305d7dc7
sscosta <43552@alunos.isel.ipl.pt>
2019-11-24 14:30:56 +0000

    changed server template for ex6

--- serie2/Resources/serie2-e-anexos/server-template.js
+++ serie2/Resources/serie2-e-anexos/server-template.js
@@ -1,17 +1,19 @@
 var fs = require('fs'); 
 var https = require('https'); 
 var options = { 
-  //key: fs.readFileSync(<ficheiro PEM com chave privada do servidor>), 
-  //cert: fs.readFileSync(<ficheiro PEM com certificado do servidor>), 
-  //ca: fs.readFileSync(<Ficheiro contendo Certificado PEM da CA root>), 
-  //requestCert: true, 
-  //rejectUnauthorized: true 
+  key: fs.readFileSync('secure-server-private.pem'), 
+  cert: fs.readFileSync('secure-server.pem'), 
+  ca: fs.readFileSync('CA1.pem'), 
+  requestCert: true, 
+  rejectUnauthorized: true 
+  
+  //rejectUnauthorized: false 
 }; 
 
 https.createServer(options, function (req, res) { 
   console.log(new Date()+' '+ 
     req.connection.remoteAddress+' '+ 
-    //req.socket.getPeerCertificate().subject.CN+' '+ 
+    req.socket.getPeerCertificate().subject.CN+' '+ 
     req.method+' '+req.url); 
   res.writeHead(200); 
   res.end("Secure Hello World with node.js\n"); 
